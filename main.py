f3a47d2bfc0e0adbd833e9c8961a10d6565c1546
diwmix <116847359+diwmix@users.noreply.github.com>
2024-02-28 12:43:20 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import math
+def main(a, b, x):
+    Z = math.sqrt(x**4+a*x+b)/math.sqrt(x**4+a*x+b)
+    print(Z, a, b, x)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main(123, 123 ,231)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
